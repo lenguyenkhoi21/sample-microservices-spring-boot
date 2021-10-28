bce5d9fff41dee0d00f30cf83371b3d5d32b64f7
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-10-28 21:13:06 +0700

    Optimize Services-a and remove @EnableScheduling for not using

--- services-a/src/main/java/com/example/servicesa/ServicesAApplication.java
+++ services-a/src/main/java/com/example/servicesa/ServicesAApplication.java
@@ -2,20 +2,17 @@ package com.example.servicesa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableScheduling
 public class ServicesAApplication {
 
     public static void main(String[] args) {
