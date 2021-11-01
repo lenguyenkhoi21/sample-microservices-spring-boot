53189b3d6b85cc441a522a0a9bc886a3a476890a
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-11-01 23:59:57 +0700

    Finish sending log to logstash

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,10 +1,7 @@
 package com.example.gateway;
 
-import com.example.gateway.config.Logstash;
-import com.example.gateway.config.AppInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -12,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
-@EnableConfigurationProperties({Logstash.class, AppInfo.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
